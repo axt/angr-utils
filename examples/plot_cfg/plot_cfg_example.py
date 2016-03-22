1543f305755410a2a989ea810a895b454efb7ed3
axt <axt@load.hu>
2016-03-22 01:01:20 +0100

    Added plot_cfg example

---
+++ examples/plot_cfg/plot_cfg_example.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import angr
+from angrutils import plot_cfg
+
+
+def analyze(b, addr, name=None):
+
+    start_state = b.factory.blank_state(addr=addr)
+    start_state.stack_push(0x0)
+    cfg = b.analyses.CFG(fail_fast=True, starts=[addr], initial_state=start_state, context_sensitivity_level=2, keep_state=True, call_depth=100)
+
+    for addr,func in proj.kb.functions.iteritems():
+        if func.name in ['main','verify']:
+            plot_cfg(cfg, "%s_%s_cfg" % (name, func.name), asminst=True, vexinst=False, func_addr={addr:True}, debug_info=False, remove_imports=True, remove_path_terminator=True)
+    
+    plot_cfg(cfg, "%s_cfg" % (name), asminst=True, vexinst=False, debug_info=False, remove_imports=True, remove_path_terminator=True)
+    
+if __name__ == "__main__":
+    proj = angr.Project("../../../angr-doc/examples/ais3_crackme/ais3_crackme", load_options={'auto_load_libs':False})
+    main = proj.loader.main_bin.get_symbol("main")
+    analyze(proj, main.addr, "ais3")
+    

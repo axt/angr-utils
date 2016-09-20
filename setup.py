b90368f441c1aec8b4543358e47ca491f140c22e
axt <axt@load.hu>
2016-09-20 22:48:30 +0200

    Bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='angr-utils',
-    version='0.0.10',
-    packages=['angrutils'],
+    version='0.1.0',
+    packages=['angrutils', 'angrutils.vis', 'angrutils.vis.angr', 'angrutils.vis.angr.x86'],
     install_requires=[
         'pydot',
         'networkx',

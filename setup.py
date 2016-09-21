ecb1f5a22c2d6ef6c8452d15e26f97bdd427f5bd
axt <axt@load.hu>
2016-09-21 03:56:27 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='angr-utils',
-    version='0.1.0',
+    version='0.1.1',
     packages=['angrutils', 'angrutils.vis', 'angrutils.vis.angr', 'angrutils.vis.angr.x86'],
     install_requires=[
         'pydot',

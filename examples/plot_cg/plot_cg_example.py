f98fe36217bdc41666e7c09659b1027150c3f837
axt <axt@load.hu>
2016-09-21 17:51:19 +0200

    consolidate callgraph example

--- examples/plot_cg/plot_cg_example.py
+++ examples/plot_cg/plot_cg_example.py
@@ -7,7 +7,7 @@ from angrutils import *
 def analyze(b, addr, name=None):
     start_state = b.factory.blank_state(addr=addr)
     start_state.stack_push(0x0)
-    cfg = b.analyses.CFGAccurate(fail_fast=True, starts=[addr], initial_state=start_state, context_sensitivity_level=2, keep_state=True, call_depth=100, normalize=True)
+    cfg = b.analyses.CFGAccurate(fail_fast=False, starts=[addr], context_sensitivity_level=1, enable_function_hints=False, keep_state=True, enable_advanced_backward_slicing=False, enable_symbolic_back_traversal=False,normalize=True)
 
     plot_cg(b.kb, "%s_callgraph" % name, format="png")
     plot_cg(b.kb, "%s_callgraph_verbose" % name, format="png", verbose=True)

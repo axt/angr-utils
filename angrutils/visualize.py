9acd00301e32de8262e3edc8fbf4d2d825c98e21
axt <axt@load.hu>
2016-09-21 03:55:52 +0200

    Using AngrVisFactory in visualize

--- angrutils/visualize.py
+++ angrutils/visualize.py
@@ -8,29 +8,14 @@ from vis.angr.x86 import *
 
 # NOTE: kept for compatibility reason
 def plot_cfg(cfg, fname, format="png", path=None, asminst=False, vexinst=False, func_addr=None, remove_imports=True, remove_path_terminator=True, debug_info=False):
-    vis = Vis()
-    vis.set_source(AngrCFGSource())
-    
-    if remove_path_terminator:
-        vis.add_transformer(AngrRemovePathTerminator())
+   
+    vis = AngrVisFactory().default_cfg_pipeline(cfg.project, asminst=asminst, vexinst=vexinst)
         
     if remove_imports:
         vis.add_transformer(AngrRemoveImports(cfg.project))
         
     if func_addr:
         vis.add_transformer(AngrFilterNodes(lambda node: node.obj.function_address in func_addr and func_addr[node.obj.function_address]))
-
-    vis.add_content(AngrCFGHead())
-    vis.add_node_annotator(AngrColorSimprocedures())
-
-    if asminst:
-        vis.add_content(AngrAsm(cfg.project))
-        
-    if vexinst:
-        vis.add_content(AngrVex(cfg.project))
-        vis.add_edge_annotator(AngrColorEdgesVex())
-    elif asminst:
-        vis.add_edge_annotator(AngrColorEdgesAsm())
     
     if debug_info:
         vis.add_content(AngrCFGDebugInfo())

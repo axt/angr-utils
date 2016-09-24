1a70aa5cfa9ce4f3da7b96086b5e5c49d9e0204f
axt <axt@load.hu>
2016-09-24 12:54:08 +0200

    Fixup of expr evaluation

--- angrutils/expr.py
+++ angrutils/expr.py
@@ -1,4 +1,5 @@
 # Expression evaluation routines
+import claripy
 
 def get_signed_range(se, expr):
     """
@@ -8,24 +9,24 @@ def get_signed_range(se, expr):
     umin = umax = smin = smax = None
     if not sat_zero(se, expr):
         try: 
-            umin = se.min(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 0])
-            umax = se.max(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 0])
+            umin = se.min(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 0])
+            umax = se.max(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 0])
             return (umin, umax)
         except:
             pass
         try: 
-            smin = -(1 << size) + se.min(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 1])
-            smax = -(1 << size) + se.max(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 1])
+            smin = -(1 << size) + se.min(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 1])
+            smax = -(1 << size) + se.max(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 1])
             return (smin, smax)
         except:
             pass
         return None
     else:
         try: 
-            umax = se.max(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 0])
+            umax = se.max(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 0])
             smin = 0
             try:
-                smin = -(1 << size) + se.min(expr, extra_constraints=[se.Extract(size-1,size-1,expr) == 1])
+                smin = -(1 << size) + se.min(expr, extra_constraints=[claripy.Extract(size-1,size-1,expr) == 1])
             except:
                 pass
             return (smin, umax)
@@ -38,7 +39,8 @@ def sat_zero(se, expr):
     return se.satisfiable(extra_constraints=([expr == 0]))
 
 def sat_negative(se, expr):
-    return se.satisfiable(extra_constraints=([se.Extract(size-1,size-1,expr) == 1]))
+    size = expr.size()
+    return se.satisfiable(extra_constraints=([claripy.Extract(size-1,size-1,expr) == 1]))
 
 def sat_positive(se, expr):
-    return se.satisfiable(extra_constraints=([se.Extract(size-1,size-1,expr) == 0]))
+    return se.satisfiable(extra_constraints=([claripy.Extract(size-1,size-1,expr) == 0]))

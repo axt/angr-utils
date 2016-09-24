4666edcc2b3072018f0d9a50eac701b191024573
axt <axt@load.hu>
2016-09-24 12:53:38 +0200

    Added plot_cdg

--- angrutils/visualize.py
+++ angrutils/visualize.py
@@ -7,7 +7,7 @@ from bingraphvis.angr import *
 from bingraphvis.angr.x86 import *
 
 def plot_cfg(cfg, fname, format="png", path=None, asminst=False, vexinst=False, func_addr=None, remove_imports=True, remove_path_terminator=True, debug_info=False):
-   
+
     vis = AngrVisFactory().default_cfg_pipeline(cfg.project, asminst=asminst, vexinst=vexinst)
         
     if remove_imports:
@@ -31,3 +31,14 @@ def plot_cg(kb, fname, format="png", verbose=False):
     vis.set_output(DotOutput(fname, format=format))    
     vis.process(kb) 
     
+
+def plot_cdg(cfg, cdg, fname, format="png", pd_edges=False, cg_edges=True, remove_fakeret=True):
+    vis = AngrVisFactory().default_cfg_pipeline(cfg.project, asminst=True, vexinst=False, color_edges=False)
+    if remove_fakeret:
+        vis.add_transformer(AngrRemoveFakeretEdges())
+    if pd_edges:
+        vis.add_transformer(AngrAddEdges(cdg.get_post_dominators(), color="green", reverse=True))
+    if cg_edges:
+        vis.add_transformer(AngrAddEdges(cdg.graph, color="purple", reverse=False))
+    vis.set_output(DotOutput(fname, format=format, show=True))    
+    vis.process(cfg.graph) 

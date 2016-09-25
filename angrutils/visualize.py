8c4847db27b885bdc3b70142c7c4ccb4a29a3d10
axt <axt@load.hu>
2016-09-25 14:20:49 +0200

    Added plot_common, plot_dfg, plot_ddg_stmt, plot_ddg_data

--- angrutils/visualize.py
+++ angrutils/visualize.py
@@ -6,23 +6,22 @@ from bingraphvis import DotOutput
 from bingraphvis.angr import *
 from bingraphvis.angr.x86 import *
 
-def plot_cfg(cfg, fname, format="png", path=None, asminst=False, vexinst=False, func_addr=None, remove_imports=True, remove_path_terminator=True, debug_info=False):
+def plot_common(graph, fname, format="png", type=True):
+    vis = AngrVisFactory().default_common_graph_pipeline(type=type)
+    vis.set_output(DotOutput(fname, format=format))
+    vis.process(graph)
 
+def plot_cfg(cfg, fname, format="png", path=None, asminst=False, vexinst=False, func_addr=None, remove_imports=True, remove_path_terminator=True, debug_info=False):
     vis = AngrVisFactory().default_cfg_pipeline(cfg.project, asminst=asminst, vexinst=vexinst)
-        
     if remove_imports:
         vis.add_transformer(AngrRemoveImports(cfg.project))
-        
     if func_addr:
         vis.add_transformer(AngrFilterNodes(lambda node: node.obj.function_address in func_addr and func_addr[node.obj.function_address]))
-    
     if debug_info:
         vis.add_content(AngrCFGDebugInfo())
-
     if path:
         vis.add_edge_annotator(AngrPathAnnotator(path))
         vis.add_node_annotator(AngrPathAnnotator(path))
-
     vis.set_output(DotOutput(fname, format=format))    
     vis.process(cfg.graph) 
 
@@ -31,7 +30,6 @@ def plot_cg(kb, fname, format="png", verbose=False):
     vis.set_output(DotOutput(fname, format=format))    
     vis.process(kb) 
     
-
 def plot_cdg(cfg, cdg, fname, format="png", pd_edges=False, cg_edges=True, remove_fakeret=True):
     vis = AngrVisFactory().default_cfg_pipeline(cfg.project, asminst=True, vexinst=False, color_edges=False)
     if remove_fakeret:
@@ -42,3 +40,36 @@ def plot_cdg(cfg, cdg, fname, format="png", pd_edges=False, cg_edges=True, remov
         vis.add_transformer(AngrAddEdges(cdg.graph, color="purple", reverse=False))
     vis.set_output(DotOutput(fname, format=format))
     vis.process(cfg.graph)
+
+def plot_dfg(dfg, fname, format="png"):
+    vis = AngrVisFactory().default_common_graph_pipeline(type=True)
+    vis.set_output(DotOutput(fname, format=format))
+    vis.process(dfg)
+
+#Note: method signature may change in the future
+def plot_ddg_stmt(ddg_stmt, fname, format="png", project=None):
+    vis = AngrVisFactory().default_common_graph_pipeline()
+    if project:
+        vis.add_content(AngrAsm(project))
+        vis.add_content(AngrVex(project))
+    vis.add_edge_annotator(AngrColorDDGStmtEdges(project))
+    vis.set_output(DotOutput(fname, format=format))
+    vis.process(ddg_stmt)
+
+#Note: method signature may change in the future
+def plot_ddg_data(ddg_data, fname, format="png", project=None, asminst=False, vexinst=True):
+    vis = Vis()
+    vis.set_source(AngrCommonSource())
+    vis.add_content(AngrDDGLocationHead())
+    vis.add_content(AngrDDGVariableHead(project=project))
+    
+    if project:
+        if asminst:
+            vis.add_content(AngrAsm(project))
+        if vexinst:
+            vis.add_content(AngrVex(project))
+    acd = AngrColorDDGData(project, labels=True)
+    vis.add_edge_annotator(acd)
+    vis.add_node_annotator(acd)
+    vis.set_output(DotOutput(fname, format=format))
+    vis.process(ddg_data)

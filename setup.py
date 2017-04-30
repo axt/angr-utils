709a46f06d037cdc18cf321125d5cf74f3278b10
axt <axt@load.hu>
2017-04-30 19:40:49 +0200

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from distutils.core import setup
 
 setup(
     name='angr-utils',
-    version='0.2.2',
+    version='0.3.0',
+    author='Attila Axt',
+    author_email='axt@load.hu',
+    license='BSD',
+    platforms=['Linux'],
     packages=['angrutils'],
     install_requires=[
         'pydot',
@@ -10,8 +14,9 @@ setup(
         'angr',
         'claripy',
         'simuvex',
-	'bingraphvis'
+        'bingraphvis >= 0.1.0'
     ],
     description='Various utilities for angr binary analysis framework',
+    long_description='Various utilities for angr binary analysis framework',
     url='https://github.com/axt/angr-utils',
 )

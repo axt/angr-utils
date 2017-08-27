f54b432586658bfdb148e020b8336b2ec0a6b086
Axt Attila <axt@load.hu>
2017-08-27 14:31:22 +0200

    Add func_graph example

---
+++ examples/plot_func_graph/README.md
@@ -0,0 +1,5 @@
+### Java_com_sun_media_sound_HeadspaceSoundbank_nOpenResourceFromByteArray
+
+![fgraph][fgraph]
+
+[fgraph]: http://i.imgur.com/Jio98Ol.png
---
+++ examples/plot_func_graph/plot_func_graph_example.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import angr
+from angrutils import plot_func_graph
+
+
+def analyze(b, name):
+    cfg = b.analyses.CFG(normalize=True)
+    for func in proj.kb.functions.values():
+        if func.name.find('Java_') == 0:
+            plot_func_graph(b, func.transition_graph, "%s_%s_cfg" % (name, func.name), asminst=True, vexinst=False)
+
+if __name__ == "__main__":
+    proj = angr.Project("../samples/1.6.26-libjsound.so", load_options={'auto_load_libs':False, 'main_opts':{'custom_base_addr':0}})
+    analyze(proj, "libjsound")
+

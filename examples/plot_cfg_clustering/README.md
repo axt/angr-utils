f3dd65f20509c25b84c4473c889bb2e39bfdeb5a
Axt Attila <axt@load.hu>
2017-08-27 14:37:02 +0200

    Add cfg clustering example

---
+++ examples/plot_cfg_clustering/README.md
@@ -0,0 +1,5 @@
+### Java_com_sun_media_sound_MixerSequencer_nAddControllerEventCallback
+
+![cfg][cfg]
+
+[cfg]: http://i.imgur.com/iyS43XN.png
---
+++ examples/plot_cfg_clustering/plot_cfg_clustering_example.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import angr
+from angrutils import plot_cfg
+
+
+def analyze(b, addr, name=None):
+    start_state = b.factory.blank_state(addr=addr)
+    start_state.stack_push(0x0)
+    cfg = b.analyses.CFGAccurate(fail_fast=True, starts=[addr], initial_state=start_state, context_sensitivity_level=2, keep_state=True, call_depth=100, normalize=True)
+    
+    plot_cfg(cfg, "%s_cfg" % (name), asminst=True, vexinst=False, debug_info=False, remove_imports=True, remove_path_terminator=True, color_depth=True)
+
+if __name__ == "__main__":
+    proj = angr.Project("../samples/1.6.26-libjsound.so", load_options={'auto_load_libs':False, 'main_opts':{'custom_base_addr':0}})
+    main = proj.loader.main_bin.get_symbol("Java_com_sun_media_sound_MixerSequencer_nAddControllerEventCallback")
+    analyze(proj, main.addr, "libjsound")
+

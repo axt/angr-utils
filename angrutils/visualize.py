85dd019598d1430b59cf1a7d81fe80c2ec91441d
Axt Attila <axt@load.hu>
2017-08-30 05:49:57 +0200

    Remove unecessary function parameter

--- angrutils/visualize.py
+++ angrutils/visualize.py
@@ -31,7 +31,7 @@ def plot_cfg(cfg, fname, format="png", path=None, asminst=False, vexinst=False, 
     vis.process(cfg.graph) 
 
 def plot_func_graph(project, graph, fname, format="png", asminst=True, ailinst=True, vexinst=False, structure=None, color_depth=False):
-    vis = AngrVisFactory().default_func_graph_pipeline(project, graph, asminst=asminst, ailinst=ailinst, vexinst=vexinst)
+    vis = AngrVisFactory().default_func_graph_pipeline(project, asminst=asminst, ailinst=ailinst, vexinst=vexinst)
     if structure:
         vis.add_clusterer(AngrStructuredClusterer(structure))
         if color_depth:

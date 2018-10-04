31eac0656dacfcb0c076137c9a9e26b4ccaddb61
axt <axt@load.hu>
2018-10-04 07:11:34 +0200

    CFGAccurate -> CFGEmulated

--- examples/plot_cfg_path/plot_cfg_path_example.py
+++ examples/plot_cfg_path/plot_cfg_path_example.py
@@ -8,7 +8,7 @@ from angrutils.exploration import NormalizedSteps
 def analyze(b, addr, name=None):
     start_state = b.factory.blank_state(addr=addr)
     start_state.stack_push(0x0)
-    cfg = b.analyses.CFGFast(fail_fast=True, function_starts=[addr], base_state=start_state, normalize=True)
+    cfg = b.analyses.CFGEmulated(fail_fast=True, starts=[addr], initial_state=start_state, context_sensitivity_level=5, keep_state=True, call_depth=100, normalize=True)
 
     plot_cfg(cfg, "%s_cfg" % (name), asminst=True, vexinst=False, debug_info=False, remove_imports=False, remove_path_terminator=False)
 
